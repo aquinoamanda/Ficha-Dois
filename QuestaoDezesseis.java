570ef4b741bddfb70544faf32180facdca1a8242
aquinoamanda <169165296+aquinoamanda@users.noreply.github.com>
2024-05-29 10:53:57 -0300

    Create QuestaoDezesseis.java

---
+++ QuestaoDezesseis.java
@@ -0,0 +1,20 @@
+public class QuestaoDezesseis {
+
+
+	public static void main(String[] args) {
+		
+		Scanner ref = new Scanner(System.in);
+		
+		System.out.println("Digite um número: ");
+		int num = ref.nextInt();
+		
+		 if ((num / 1 == 0 ) && (num / num == 0)){
+		System.out.println("o número é primo");
+		 }
+		 
+		 else
+			System.out.println("o número é primo");
+		 
+	
+	}
+}
